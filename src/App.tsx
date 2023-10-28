4f61845b7ddac1e096bc55271ef5eb7d6a9ebc3b
Flávio Eduardo <xflax00@gmail.com>
2023-10-28 19:51:36 -0300

    add configuration

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import './App.css'
+import Header from './components/Header/Header'
+import Purchases from './components/Purchases/Purchases'
+
+function App() {
+
+  return (
+    <>
+      <Header />
+      <main>
+        <Purchases />
+      </main>
+    </>
+  )
+}
+
+export default App
