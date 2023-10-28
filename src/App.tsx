54969efa0ec0d4cd099e48e03b1754abf79c7624
Flávio Eduardo <xflax00@gmail.com>
2023-10-28 20:10:27 -0300

    add new purchase button

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import './App.css'
 import Header from './components/Header/Header'
+import PurchaseButton from './components/PurchaseButton/PurchaseButton'
 import Purchases from './components/Purchases/Purchases'
 
 function App() {
@@ -9,7 +10,9 @@ function App() {
       <Header />
       <main>
         <Purchases />
+        <PurchaseButton />
       </main>
+
     </>
   )
 }
---
+++ src/components/PurchaseButton/PurchaseButton.tsx
@@ -0,0 +1,13 @@
+import "./PurchaseButton.scss"
+
+import { GrAdd } from "react-icons/gr"
+
+const PurchaseButton = () => {
+  return (
+    <button className="purchase_btn">
+      <GrAdd></GrAdd>
+    </button>
+  )
+}
+
+export default PurchaseButton
